959e5f39f689cdcb061bfc8765d728856364a10a
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2023-03-24 19:21:57 +0000

    Add peephole to aten::device(str, int)

--- torch/onnx/symbolic_opset17.py
+++ torch/onnx/symbolic_opset17.py
@@ -65,13 +65,6 @@ def _compute_edge_sizes(n_fft, window_size):
     return left, right
 
 
-# @_onnx_symbolic("aten::device")
-# # @symbolic_helper.parse_args("s", "i")
-# def device(g: jit_utils.GraphContext, type, index):
-#     # device as a noop
-#     return type
-
-
 @_onnx_symbolic("aten::stft")
 @symbolic_helper.parse_args("v", "i", "i", "i", "v", "b", "b", "b")
 @_beartype.beartype
